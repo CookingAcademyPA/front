4deed881a00e6ab4012747db3ac533fa0c83c0f0
MrPuel <theo.gouraud@inetum.com>
2023-10-16 23:03:03 +0200

    add modal to buy

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  showModal: boolean = false;
   cartItems: any[] = [
     {
       name: 'Produit 1',
@@ -23,8 +24,24 @@ export class CartComponent {
       price: 25.0,
     },
   ];
+  cartSummary: string = '';
+  totalPrice: number = 0;
+  deliveryAddress: string = '123 Main Street, City';
   // Calcule le total du panier
   calculateTotal(): number {
     return this.cartItems.reduce((total, item) => total + item.price, 0);
+  }
+  openModal(): void {
+    this.showModal = true;
+    this.calculateSummary();
+  }
+
+  // Méthode pour fermer la modal
+  closeModal(): void {
+    this.showModal = false;
+  }
+  calculateSummary(): void {
+    this.cartSummary = this.cartItems.map(item => `${item.name} - ${item.price}`).join(', ');
+    this.totalPrice = this.cartItems.reduce((total, item) => total + item.price, 0);
   }
 }
