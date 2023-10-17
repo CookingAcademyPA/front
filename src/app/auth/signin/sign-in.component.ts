38b007c62a0a4d09f1e45326cfc33720cecc453a
MrPuel <theo.gouraud@inetum.com>
2023-10-17 21:55:04 +0200

    add login

--- src/app/auth/signin/sign-in.component.ts
+++ src/app/auth/signin/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private router: Router, private http: HttpClient) {
+  }
+
+  onSubmit(): void {
+    const formData = {
+      email: this.email,
+      password: this.password
+    };
+
+    // En-têtes pour la requête HTTP (si nécessaire)
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+      // Ajoutez d'autres en-têtes si nécessaires
+    });
+
+    this.http.post<any>('https://cookingacademy.azurewebsites.net/api/auth/login', formData, { headers })
+      .subscribe(
+        (response) => {
+          // En cas de succès, le serveur renvoie un token.
+          // Vous pouvez stocker ce token dans un service d'authentification ou dans un cookie sécurisé.
+
+          // Redirigez l'utilisateur vers la page d'accueil (/home).
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          // En cas d'échec de la connexion, gérez l'erreur ici (par exemple, affichez un message d'erreur à l'utilisateur).
+          this.errorMessage = 'Erreur de connexion. Veuillez vérifier vos informations.';
+          console.error('Erreur de connexion :', error);
+        }
+      );
+  }
+
+
 
 }
