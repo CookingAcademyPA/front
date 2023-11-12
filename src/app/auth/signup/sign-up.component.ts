31ffc75ad07758875ee2e1186380d66e6b267499
MrPuel <theo.gouraud@inetum.com>
2023-11-12 22:08:24 +0100

    change redirection for subscription

--- src/app/auth/signup/sign-up.component.ts
+++ src/app/auth/signup/sign-up.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {environment} from "../../../environments/environment";
 
-;
 
 @Component({
   selector: 'app-signup',
@@ -11,6 +11,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit{
+  private apiUrl = environment.apiUrl;
   registerForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
@@ -42,20 +43,18 @@ export class SignUpComponent implements OnInit{
       'is_admin': false,
       ...this.registerForm.value
     };
-    this.http.post('https://cookingacademy.azurewebsites.net/api/auth/signup', requestBody)
+    this.http.post(`${this.apiUrl}/auth/signup`, requestBody)
       .subscribe(
         (response: any) => {
           // L'inscription a réussi, vous pouvez gérer ici la redirection et la notification
           console.log('Inscription réussie:', response);
 
           // Redirigez vers la page d'accueil
-          this.router.navigate(['/home']);
+          this.router.navigate(['/signin']);
 
           // Affichez une notification
-          // Vous pouvez utiliser une librairie comme ngx-toastr pour les notifications
         },
         (error: any) => {
-          // L'inscription a échoué, gérer les erreurs ici
           console.error('Erreur lors de l\'inscription:', error);
         }
       );
